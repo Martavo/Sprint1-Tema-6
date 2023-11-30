0c7161e805f5a27f30caa4b5b79391e1222adbd7
Marta Villalba <marta.villalba.orta@gmail.com>
2023-11-30 10:45:38 +0100

    Revisión ejercicios

--- Nivel 1/Ejercicio 2/ProcesarFormulario.php
+++ Nivel 1/Ejercicio 2/ProcesarFormulario.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 
-if ($_SERVER["REQUEST_METHOD"] == "GET") { // Verificamos que se use el método GET
-    $_SESSION["nombre"] = isset($_GET['nombre']) ? $_GET['nombre'] : ""; //con el isset se verifica que la variable este definida, sino seria false y se añadiria "" a la variable
-    $_SESSION["apellido"] = isset($_GET['apellido']) ? $_GET['apellido'] : "";
-    $_SESSION["usuario"] = isset($_GET['usuario']) ? $_GET['usuario'] : "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") { // Verificamos que se use el método POST
+    $_SESSION["nombre"] = isset($_POST['nombre']) ? $_POST['nombre'] : ""; //con el isset se verifica que la variable este definida, sino seria false y se añadiria "" a la variable
+    $_SESSION["apellido"] = isset($_POST['apellido']) ? $_POST['apellido'] : "";
+    $_SESSION["usuario"] = isset($_POST['usuario']) ? $_POST['usuario'] : "";
 
     echo "<h2>Valores añadidos al formulario:</h2>"; 
     echo "<p>Nombre: {$_SESSION['nombre']}</p>"; //a través del array asociativo accedemos a la variable de nombre
